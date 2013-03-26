8598c1682f15d5e07c928113279e644cf8ba14d8
ankem5 <ankem5@gmail.com>
2013-03-26 10:00:49 +0530

    message2

--- GitClass.java
+++ GitClass.java
@@ -3,5 +3,6 @@ package com.sms;
 public class GitClass {
 	public static void main(String[] args) {
 		System.out.println("This is my Git Test");
+		System.out.println("This is my Git Test");
 	}
 }
